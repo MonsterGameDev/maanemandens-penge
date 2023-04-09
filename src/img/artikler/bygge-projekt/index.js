a004426d5500db24d1b6b04dd97f193ea0a9966f
MonsterGameDev <per@brf.dk>
2023-04-09 14:44:36 +0200

    assets

---
+++ src/img/artikler/bygge-projekt/index.js
@@ -0,0 +1,14 @@
+import IMG_1 from './w1-1.webp';
+import IMG_2 from './w1-2.webp';
+import IMG_3 from './w1-4.webp';
+import IMG_4 from './w1-5.webp';
+import IMG_5 from './w1-6.webp';
+import IMG_6 from './w1-7.webp';
+import IMG_7 from './w2-1.webp';
+import IMG_8 from './w2-3.webp';
+import IMG_9 from './w2-4.webp';
+import IMG_10 from './d-teater-m-scene.webp';
+
+export {
+    IMG_1, IMG_2, IMG_3, IMG_4, IMG_5, IMG_6, IMG_7, IMG_8, IMG_9, IMG_10
+}
---
+++ src/img/artikler/mauriske-proscenium/index.js
@@ -0,0 +1,15 @@
+import LASER from './laser-cutter.webp';
+import LAG_2 from './layer-2';
+import LAG_3 from './layer-3';
+import LAG_4 from './layer-4';
+import LAYERS_1 from './layers-1';
+import LAYERS_2 from './layers-2';
+import LAYERS_3 from './layers3';
+import LAYERS_4 from './layers-4';
+import LAYERS_5 from './layers-5';
+import BANNER from './banner-wood-all';
+import CARDBOARD_ALL from './cardbord-all';
+
+export {
+    LAYERS_1, LAYERS_2, LAYERS_3, LAYERS_4, LAYERS_5, LAG_2, LAG_3, LAG_4, LASER, BANNER, CARDBOARD_ALL
+}
