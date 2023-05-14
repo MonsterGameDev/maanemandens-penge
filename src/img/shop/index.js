53a907ebc4bfccf72bac7f1760ebc711151aff34
MonsterGameDev <per@brf.dk>
2023-05-14 15:01:27 +0200

    barrel til images

---
+++ src/img/shop/index.js
@@ -0,0 +1,25 @@
+import ARK_01_02 from './Ark 01-02.webp';
+import ARK_03_04 from './Ark 03-04.webp';
+import ARK_05_06 from './Ark 05-06.webp';
+import ARK_07_08 from './Ark 07-08.webp';
+import ARK_09_10 from './Ark 09-10.webp';
+import ARK_11_12 from './Ark 11-12.webp';
+import ARK_13_14 from './Ark 13-14.webp';
+import ARK_15_16 from './Ark 15-16.webp';
+import ARK_17_18 from './Ark 17-18.webp';
+import ARK_19_20 from './Ark 19-20.webp';
+import ARK_21_22 from './Ark 21-22.webp';
+import ARK_23_24 from './Ark 23-24.webp';
+import ARK_25_26 from './Ark 25-26.webp';
+import ARK_27_28 from './Ark 27-28.webp';
+import ARK_29_30 from './Ark 29-30.webp';
+import ARK_31_32 from './Ark 31-32.webp';
+import ARK_33_34 from './Ark 33-34.webp';
+import FIGURER_1 from './Figurark 1.webp'
+import FIGURER_2 from './Figurark 2.webp'
+import FIGURER_3 from './Figurark 3.webp'
+import FIGURER_4 from './Figurark 4.webp'
+import PROSCENIUM_M_LOGGIA from './D-teater, ark 1.webp';
+import DRAPERIER from './D-teater, ark 2.webp';
+
+export { ARK_01_02, ARK_03_04, ARK_05_06, ARK_07_08, ARK_09_10, ARK_11_12, ARK_13_14, ARK_15_16, ARK_17_18, ARK_19_20, ARK_21_22, ARK_23_24, ARK_25_26, ARK_27_28, ARK_29_30, ARK_31_32, ARK_33_34, FIGURER_1, FIGURER_2, FIGURER_3, FIGURER_4, DRAPERIER, PROSCENIUM_M_LOGGIA }
